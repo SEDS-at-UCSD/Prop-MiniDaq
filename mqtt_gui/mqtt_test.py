126f5a781b0041901b4d4d78f61bfe337ad0843c
Anchit Kumar <ank016@ucsd.edu>
2023-10-29 15:49:36 -0700

    working live update data acquisition
    - fetches live data from functional mqtt server over websocket and basic html display

--- mqtt_gui/mqtt_test.py
+++ mqtt_gui/mqtt_test.py
@@ -9,9 +9,10 @@ from threading import Event
 # MQTT configuration
 mqtt_broker_address = "localhost"
 mqtt_topic_serial = "serial_data"
-mqtt_topic_log = "log_data"
+mqtt_log_1015 = "log_data_1015"
+mqtt_log_1115 = "log_data_1115"
 
-client = mqtt.Client("web_server")
+client = mqtt.Client("propdaq")
 
 # Replace 'COMX' with the actual COM port where your Arduino or device is connected.
 ser = serial.Serial('/dev/cu.usbserial-0001', 921600)  # Adjust the baud rate as needed.
@@ -24,18 +25,15 @@ raw_log_file = open('raw_serial_log.txt', 'a')
 event = Event()
 read_event = Event()
 
-datatopass = [""]
+datatopass = ["", ""]
 data_lock = threading.Lock()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker with result code "+str(rc))
     client.subscribe(mqtt_topic_serial)
 
-def on_message(client, userdata, msg):
-    data = msg.payload.decode('utf-8')
-    if msg.topic == mqtt_topic_serial:
-        with data_lock:
-            datatopass[0] = data
+def on_message(client, userdata, message):
+    print(f"Received message on topic '{message.topic}': {message.payload.decode('utf-8')}")
 
 def read_serial_and_log_high_freq():
     while True:
@@ -43,36 +41,37 @@ def read_serial_and_log_high_freq():
             # Read a line of data from the Serial Monitor
             data = ser.readline().decode('utf-8').strip()
             data_dict = json.loads(data)
+            #print (data)
+            #print(data_dict)
             # Log the data to a text file
             data_formatted = (
-                str(datetime.now())[11:]
+                str(datetime.now())[11:] 
                 + " "
                 + str(data_dict['BoardID'])
                 + "  "
-                + str(data_dict['Sensors'][0])
-                + "  "
-                + str(data_dict['Sensors'][1])
-                + "  "
-                + str(data_dict['Sensors'][2])
-                + "  "
-                + str(data_dict['Sensors'][3])
-                + "\r\n"
-            )
+                + str(data_dict['SensorType'])
+                + "  ")
+            for i in range(len(data_dict['Sensors'])):
+                data_formatted += str(data_dict['Sensors'][i]) + "  "
+            #print (data_formatted)
+            
             with data_lock:
                 datatopass[0] = data_formatted
-
-            raw_log_file.write(data_formatted)
-            raw_log_file.flush()  # Flush the buffer to ensure data is written immediately
+                datatopass[1] = data_dict['SensorType']
+                raw_log_file.write(data_formatted)
+                raw_log_file.flush()  # Flush the buffer to ensure data is written immediately
 
         except Exception as e:
-            print(f"Serial read error: {e}")
+            print(f"Serial read error: {e}, {data}")
 
 def publish_data():
-    while True:
+
         with data_lock:
-            if datatopass[0]:
-                client.publish(mqtt_topic_log, datatopass[0])
-        time.sleep(0.5)
+            if datatopass[1] == 'ADS1015':
+                client.publish(mqtt_log_1015, datatopass[0])
+            elif datatopass[1] == 'ADS1115':
+                client.publish(mqtt_log_1115, datatopass[0])
+        
 
 def main():
     client.on_connect = on_connect
