594b046db2c446afa102a1574fbe37ccf2767e7e
Anchit Kumar <ank016@ucsd.edu>
2023-11-08 16:36:55 -0800

    file management
    - renamed mqtt_test.py to main.py
    - old main.py with flask server is now flask_test.py
    - removed notes.txt
    - added npm to requirements.txt
    - removed gui_serial.txt

---
+++ mqtt_gui/flask_test.py
@@ -0,0 +1,94 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import serial
+import time
+import json
+from datetime import datetime
+import threading 
+from threading import Event
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+# Replace 'COMX' with the actual COM port where your Arduino or device is connected.
+ser = serial.Serial('/dev/cu.usbserial-0001', 921600)  # Adjust the baud rate as needed.
+
+# Open a text file for appending
+gui_log_file = open('gui_serial.txt', 'w')
+gui_log_file_read = open('gui_serial.txt', 'r')
+raw_log_file = open('raw_serial_log.txt', 'a')
+
+
+
+@socketio.on('connect', namespace='/')
+def connect():
+    print('Client connected')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+event = Event()
+read_event = Event()
+
+datatopass = [""]
+
+
+
+def read_serial_and_log_high_freq():
+    while True:
+        try:
+            # Read a line of data from the Serial Monitor
+            data = ser.readline().decode('utf-8').strip()
+            data_dict = json.loads(data)
+            #print (data_dict)
+            # Log the data to a text file
+            data_formatted = str(datetime.now())[11:] + " " + str(data_dict['BoardID']) + "  " + str(data_dict['SensorType']) + "  " + str(data_dict['Sensors'][0]) + "  " + str(data_dict['Sensors'][1]) + "  " + str(data_dict['Sensors'][2]) + "  " + str(data_dict['Sensors'][3]) + "\r\n"
+            if event.is_set:
+                datatopass[0] = data_formatted
+                
+                read_event.set
+            #print (data_dict)
+            raw_log_file.write(data_formatted)
+            raw_log_file.flush()  # Flush the buffer to ensure data is written immediately
+
+        except Exception as e:
+            print(f"Serial read error: {e}")
+
+
+def read_serial_and_log():
+    while True:
+        try:
+            event.set
+            #while (read_event.is_set != True):
+            time.sleep(0.05)
+            print(datatopass[0] + '<br>\n')
+            gui_log_file.write(datatopass[0])
+            gui_log_file.flush()  # Flush the buffer to ensure data is written immediately
+            socketio.emit('serial_data', datatopass[0])
+            with open('gui_serial.txt', 'r') as log:
+                data = log.read().replace('\r\n', '<br>')
+                print(data)
+            socketio.emit('log_data', data)
+            time.sleep(0.5)
+            print("data to GUI")
+        except Exception as e:
+            print(f"Read log file error: {e}")
+
+
+
+if __name__ == '__main__':
+    #socketio.start_background_task(target=read_serial_and_log)
+    #socketio.start_background_task(target=read_log_file)
+    #socketio.start_background_task(target=read_serial_and_log_high_freq)
+
+    t1 = threading.Thread(target=read_serial_and_log)
+    #t2 = threading.Thread(target=read_log_file)
+    t3 = threading.Thread(target=read_serial_and_log_high_freq)
+    
+    t1.start()
+    #t2.start()
+    t3.start()
+    socketio.run(app, host='0.0.0.0', port=4000)
--- mqtt_gui/main.py
+++ mqtt_gui/main.py
@@ -1,41 +1,49 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+import paho.mqtt.client as mqtt
 import serial
 import time
 import json
 from datetime import datetime
-import threading 
+import threading
 from threading import Event
 
 
-app = Flask(__name__)
-socketio = SocketIO(app)
+# MQTT configuration
+mqtt_broker_address = "localhost"
+mqtt_topic_serial = "serial_data"
+b1_mqtt_log_1015 = "b1_log_data_1015"
+b1_mqtt_log_1115 = "b1_log_data_1115"
+
+
+with open('cf_config.json', 'r') as json_file:
+    conv_configs = json.load(json_file)
+
+cf_1015 = conv_configs['Conv_Factor_ADS1015']
+cf_1115 = conv_configs['Conv_Factor_ADS1115']
+
+
+client = mqtt.Client("propdaq")
 
 # Replace 'COMX' with the actual COM port where your Arduino or device is connected.
 ser = serial.Serial('/dev/cu.usbserial-0001', 921600)  # Adjust the baud rate as needed.
 
 # Open a text file for appending
 gui_log_file = open('gui_serial.txt', 'w')
-gui_log_file_read = open('gui_serial.txt', 'r')
 raw_log_file = open('raw_serial_log.txt', 'a')
-
-
-
-@socketio.on('connect', namespace='/')
-def connect():
-    print('Client connected')
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 event = Event()
 read_event = Event()
 
-datatopass = [""]
+datatopass = ["", "", ""]
+data_lock = threading.Lock()
 
 
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code "+str(rc))
+    client.subscribe(mqtt_topic_serial)
+
+def on_message(client, userdata, message):
+    print(f"Received message on topic '{message.topic}': {message.payload.decode('utf-8')}")
 
 def read_serial_and_log_high_freq():
     while True:
@@ -43,52 +51,77 @@ def read_serial_and_log_high_freq():
             # Read a line of data from the Serial Monitor
             data = ser.readline().decode('utf-8').strip()
             data_dict = json.loads(data)
-            #print (data_dict)
+            
+            #print(data_dict)
             # Log the data to a text file
-            data_formatted = str(datetime.now())[11:] + " " + str(data_dict['BoardID']) + "  " + str(data_dict['SensorType']) + "  " + str(data_dict['Sensors'][0]) + "  " + str(data_dict['Sensors'][1]) + "  " + str(data_dict['Sensors'][2]) + "  " + str(data_dict['Sensors'][3]) + "\r\n"
-            if event.is_set:
+            
+            data_formatted = (
+                str(datetime.now())[11:] 
+                + " "
+                + str(data_dict['BoardID'])
+                + "  "
+                + str(data_dict['SensorType'])
+                + "  ")
+            for i in range(len(data_dict['Sensors'])):
+                data_formatted += str(data_dict['Sensors'][i]) + "  "
+            
+
+            #print (data_formatted)
+            #print (publish_json)
+            #print(data_dict)
+            #print (datatopass)
+
+                        
+            
+            with data_lock:
                 datatopass[0] = data_formatted
-                
-                read_event.set
-            #print (data_dict)
-            raw_log_file.write(data_formatted)
-            raw_log_file.flush()  # Flush the buffer to ensure data is written immediately
+                datatopass[1] = data_dict['SensorType']
+                publish_json = (
+                "{\"time\": \"" + str(datetime.now())[11:19] 
+                + "\","
+                + "\"sensor_readings\": "
+                )
+                converted_values = []
+                for i in range(len(data_dict['Sensors'])):
+                    if datatopass[1] == 'ADS1015':
+                        converted_values.append(round(data_dict['Sensors'][i] * cf_1015, 1))
+                    else:
+                        converted_values.append(round(data_dict['Sensors'][i] * cf_1115, 1))
+            
+                publish_json += str(converted_values)
+                publish_json += '}'
+                datatopass[2] = publish_json
+                raw_log_file.write(data_formatted)
+                raw_log_file.flush()  # Flush the buffer to ensure data is written immediately
 
         except Exception as e:
-            print(f"Serial read error: {e}")
+            print(f"Serial read error: {e}, {data}")
+            
 
-
-def read_serial_and_log():
+def publish_data():
     while True:
-        try:
-            event.set
-            #while (read_event.is_set != True):
-            time.sleep(0.05)
-            print(datatopass[0] + '<br>\n')
-            gui_log_file.write(datatopass[0])
-            gui_log_file.flush()  # Flush the buffer to ensure data is written immediately
-            socketio.emit('serial_data', datatopass[0])
-            with open('gui_serial.txt', 'r') as log:
-                data = log.read().replace('\r\n', '<br>')
-                print(data)
-            socketio.emit('log_data', data)
-            time.sleep(0.5)
-            print("data to GUI")
-        except Exception as e:
-            print(f"Read log file error: {e}")
+        time.sleep(0.1);
+        with data_lock:
+            if datatopass[1] == 'ADS1015':
+                client.publish(b1_mqtt_log_1015, datatopass[2])
+                #print(datatopass)
+            if datatopass[1] == 'ADS1115':
+                client.publish(b1_mqtt_log_1115, datatopass[2])
+                #print(datatopass)
+        
 
+def main():
+    client.on_connect = on_connect
+    client.on_message = on_message
 
+    client.connect(mqtt_broker_address, 1883, 60)
+    client.loop_start()
+
+    t1 = threading.Thread(target=read_serial_and_log_high_freq)
+    t2 = threading.Thread(target=publish_data)
+
+    t1.start()
+    t2.start()
 
 if __name__ == '__main__':
-    #socketio.start_background_task(target=read_serial_and_log)
-    #socketio.start_background_task(target=read_log_file)
-    #socketio.start_background_task(target=read_serial_and_log_high_freq)
-
-    t1 = threading.Thread(target=read_serial_and_log)
-    #t2 = threading.Thread(target=read_log_file)
-    t3 = threading.Thread(target=read_serial_and_log_high_freq)
-    
-    t1.start()
-    #t2.start()
-    t3.start()
-    socketio.run(app, host='0.0.0.0', port=4000)
+    main()
