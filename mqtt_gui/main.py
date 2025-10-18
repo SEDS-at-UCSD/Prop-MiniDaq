8b3fd54ecba4da15058c967bf2594fd39b68d9d1
hdyuen <hoyuen@ucsd.edu>
2025-10-18 13:37:28 -0700

    implementing arming of sensor board
    wip v1

--- mqtt_gui/main.py
+++ mqtt_gui/main.py
@@ -30,7 +30,7 @@ app.add_middleware(
 )
 
 # Define the range of boards
-SWITCH_BOARD_RANGE = [1,2,6,7,8] #DRIVERS
+SWITCH_BOARD_RANGE = [1,2,6,7,8,9] #DRIVERS
 ANAL_BOARD_RANGE = [3,4,5] #DATA
 
 # Dynamically generate conversion factors and add factors from JSON config
